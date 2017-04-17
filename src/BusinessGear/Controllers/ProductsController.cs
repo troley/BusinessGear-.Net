cb64c8f39d26b98ae4e1e9e927b137f86dca2bcb
troley <rene.uhliar@gmail.com>
2017-04-18 01:23:42 +0200

    product CRUD scaffolded & secured

--- src/BusinessGear/Controllers/ProductsController.cs
+++ src/BusinessGear/Controllers/ProductsController.cs
@@ -1,25 +1,195 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BusinessGear.Data;
+using BusinessGear.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace BusinessGear.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly ApplicationDbContext _context;
 
-        // GET: /Products/
-        public IActionResult Index()
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
         {
             if (Request.Cookies["loggedIn"] == null)
             {
                 return RedirectToAction("Login", "Admin");
             }
 
+            refreshCookie();
+            return View(await _context.Products.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (Request.Cookies["loggedIn"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            refreshCookie();
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            if (Request.Cookies["loggedIn"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+
+            refreshCookie();
 
             return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,Description")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (Request.Cookies["loggedIn"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.SingleOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            refreshCookie();
+
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,Description")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (Request.Cookies["loggedIn"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            refreshCookie();
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+
+        private void refreshCookie()
+        {
+            Response.Cookies.Delete("loggedIn");
+            CookieOptions options = new CookieOptions {Expires = DateTime.Now.AddMinutes(10)};
+            Response.Cookies.Append("loggedIn", "loggedIn", options);
         }
     }
 }
